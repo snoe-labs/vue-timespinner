11723e17cc6e9a330adbbff99260bf0468f1726c
Gregory Plüss <greg@gregorypluess.ch>
2021-05-11 22:44:47 +0200

    Add project boilerplate

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import VueTimespinner from '@/vue-timespinner.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueTimespinner
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-timespinner />
+  </div>
+</template>
